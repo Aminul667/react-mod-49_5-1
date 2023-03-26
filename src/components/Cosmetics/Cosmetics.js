b85befeb29a0c2171a42297674986fcf795ef28c
Aminul667 <rahathssn@gmail.com>
2023-03-26 13:44:45 +0600

    49_5-3 Create and Load Fake data, json generator, online api

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { add } from '../../utilities/calculate';
+import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first, second);
-
+    const cosmetics = [
+        {id:1, name:'Item 1', price:100},
+        {id:2, name:'Item 2', price:200},
+        {id:3, name:'Item 3', price:300},
+        {id:4, name:'Item 4', price:400},
+        {id:5, name:'Item 5', price:500}
+    ]
     return (
         <div>
             <h1>Welcome to the Store</h1>
-            <p>Total: {total}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic 
+                    cosmetic={cosmetic} 
+                    key={cosmetic.id}></Cosmetic>)
+            }
         </div>
     );
 };
